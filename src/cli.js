9bd07ecac79ff1afaa07a82faf86c8c0f190e198
Ronny Visa <ronny.visa@tranzact.net>
2019-10-23 21:45:32 -0500

    refactored cli.js and mdlink.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,16 @@
-#!/usr/bin/env node
+#!/usr/bin/env node'use sctrict';
+const commander = require('commander');
+
 const cli = require('../src/mdlinks-cli.js');
 
-const [, , route, option1, option2] = process.argv;
+const program = new commander.Command();
+program.version('1.0.0').description('Statistics about markdown files');
+program.name('md-links').usage('[options]');
+program.option('-r, --route <type>', 'Muestra el total de links, el total de links únicos y rotos');
+program.option('-v, --validate', 'Valida todos los links dentro de un archivo MD.');
+program.option('-s, --stats', 'Muestra el total de links y el total de links únicos.');
+program.parse(process.argv);
+// const [, , route, option1, option2] = process.argv;
+const options = program.opts();
 // eslint-disable-next-line no-console
-cli.showCli(route, option1, option2).then((result) => console.log(result));
+cli.showCli(options).then((result) => console.log(result)).catch((error) => console.log(error));
--- src/mdlinks-cli.js
+++ src/mdlinks-cli.js
@@ -1,43 +1,32 @@
-const commander = require('commander');
+// const commander = require('commander');
 const chalk = require('chalk');
 const mdLink = require('../src/md-links.js');
 const stat = require('../src/stats.js');
 
-const program = new commander.Command();
-program.version('2.0.0').description('Stadistics about markdown files');
-program
-  .name('md-links')
-  .usage('[options]')
-  .option('--validate', 'Valida todos los links dentro de un archivo MD.')
-  .option('--stats', 'Muestra el total de links y el total de links únicos.')
-  .option('--stats --validate', 'Muestra el total de links, el total de links únicos y rotos');
-program.parse(process.argv);
+// const getOption = (option1, option2) => {
+//   if (option1 === '--validate' && option2 === undefined) {
+//     return { validate: true };
+//   }
+//   if (option1 === '--stats' && option2 === '--validate') {
+//     return { validate: true };
+//   }
+//   return { validate: false };
+// };
 
-const getOption = (option1, option2) => {
-  if (option1 === '--validate' && option2 === undefined) {
-    return { validate: true };
-  }
-  if (option1 === '--stats' && option2 === '--validate') {
-    return { validate: true };
-  }
-  return { validate: false };
-};
-
-const showCli = (route, option1, option2) => {
-  const validate = getOption(option1, option2);
-  return mdLink.mdLinks(route, validate)
+const showCli = (options) => {
+  mdLink.mdLinks(options.route, options)
     .then((response) => {
       let output = '';
       if (response.length === 0) {
         output += chalk.yellow('No se encontraron links o archivos md.');
       }
-      if (option1 === '--stats' && option2 === '--validate') {
+      if ((options.stats) && (options.validate)) {
         output = `\n${chalk.cyan('Total: ')} ${response.length} \n${chalk.cyan('Unique: ')} ${stat.uniquesLinks(response)} \n${chalk.cyan('Broken: ')} ${stat.brokenLinks(response)}`;
       }
-      if (option1 === '--stats' && option2 === undefined) {
+      if ((options.stats) && (!options.validate)) {
         output = `\n${chalk.cyan('Total: ')} ${response.length} \n${chalk.cyan('Unique: ')} ${stat.uniquesLinks(response)}`;
       }
-      if (option1 === '--validate' && option2 === undefined) {
+      if ((!options.stats) && (options.validate)) {
         response.forEach((objectLink) => {
           if (objectLink.statusText === 'OK') {
             output += `\n${chalk.bgBlue.black(objectLink.path)} ${chalk.magenta(objectLink.href)} ${chalk.green(objectLink.status)} ${chalk.bgGreen.black(objectLink.statusText)} ${chalk.yellow(objectLink.text)}`;
@@ -46,12 +35,12 @@ const showCli = (route, option1, option2) => {
           }
         });
       }
-      if (option1 === undefined) {
+      if ((!options.stats) && (!options.validate)) {
         response.forEach((objectLink) => {
           output += `\n${chalk.bgBlue.black(objectLink.path)} ${chalk.magenta(objectLink.href)} ${chalk.yellow(objectLink.text)}`;
         });
       }
-      if (option1 !== '--stats' && option1 !== '--validate' && option1 !== undefined) {
+      if ((!options.stats) && (!options.validate) && (!options.route)) {
         output = chalk.red('No se encontró el comando. Usa md-links --help para recibir información.');
       }
       return output;
